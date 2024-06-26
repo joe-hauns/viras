7ae2fde79438949585f51d8e3d3046d5033fee3b
joe-hauns <joe-hauns@protonmail.com>
2024-06-26 08:17:22 +0200

    tidy

--- src/viras.h
+++ src/viras.h
@@ -15,7 +15,7 @@
 namespace viras {
 
   template<class C>
-  class Viras 
+  class Viras
   {
 
     template<class Conf>
@@ -46,14 +46,14 @@ namespace viras {
     // TODO make it possible to fail gracefully with uniterpreted stuff
 
   private:
-    std::vector<LiraLiteral<C>> analyse(Literals<C> const& self, Var<C> x) 
-    { 
-      return iter::array(self) 
+    std::vector<LiraLiteral<C>> analyse(Literals<C> const& self, Var<C> x)
+    {
+      return iter::array(self)
         | iter::map([&](auto lit) { return LiraLiteral<C>::analyse(lit, x); })
         | iter::collect_vec;
     }
 
-    std::vector<LiraLiteral<C>> analyse(typename C::Literals const& self, typename C::Var x) 
+    std::vector<LiraLiteral<C>> analyse(typename C::Literals const& self, typename C::Var x)
     { return analyse(Literals<C> { &_config, self }, Var<C> { &_config, x }); }
 
     static constexpr Epsilon epsilon;
@@ -65,7 +65,7 @@ namespace viras {
       auto isIneq = [](auto symbol) { return (symbol == PredSymbol::Geq || symbol == PredSymbol::Gt); };
       using VT = VirtualTerm<C>;
 
-      return iter::if_then_(t.breaks.empty(), 
+      return iter::if_then_(t.breaks.empty(),
                            iter::if_then_(t.sslp == 0              , iter::vals<VT>(-infty))
                                  else_if_(symbol == PredSymbol::Neq, iter::vals<VT>(-infty, t.zero(term(0)) + epsilon))
                                  else_if_(symbol == PredSymbol:: Eq, iter::vals<VT>(t.zero(term(0))))
@@ -75,14 +75,14 @@ namespace viras {
                                  | iter_dbg(1, "lra term")
                                  )
 
-                   else_is_(!t.breaks.empty(), [&]() { 
+                   else_is_(!t.breaks.empty(), [&]() {
                        auto ebreak       = [&]() {
-                       return 
-                         iter::if_then_(t.periodic(), 
-                                        iter::array(t.breaks) 
+                       return
+                         iter::if_then_(t.periodic(),
+                                        iter::array(t.breaks)
                                           | iter::map([&](auto* b) { return VT(*b); }) )
 
-                               else____(iter::array(t.breaks) 
+                               else____(iter::array(t.breaks)
                                           | iter::flat_map([&](auto* b) { return intersectGrid(*b, Bound::Open, t.distXminus(), t.deltaX(), Bound::Open); })
                                           | iter::map([](auto t) { return VT(t); }) )
                        | iter_dbg(1, "ebreak")
@@ -91,25 +91,24 @@ namespace viras {
                        auto breaks_plus_epsilon = [&]() { return ebreak() | iter::map([](auto vt) { return vt + epsilon; })
                        ; };
 
-                       auto ezero = [&]() { 
-                       return 
-                          iter::if_then_(t.periodic(), 
-                                         iter::array(t.breaks) 
+                       auto ezero = [&]() {
+                       return
+                          iter::if_then_(t.periodic(),
+                                         iter::array(t.breaks)
                                            | iter::map([&](auto* b) { return VT::periodic(t.zero(b->t), b->p); }))
 
                                 else_if_(t.oslp == t.sslp,
-                                         iter::array(t.breaks) 
+                                         iter::array(t.breaks)
                                            | iter::map([&](auto* b) { return VT(t.zero(b->t)); }))
 
-                                else____(iter::array(t.breaks) 
+                                else____(iter::array(t.breaks)
                                            | iter::flat_map([&](auto* b) { return intersectGrid(Break<C>(t.zero(b->t), 1 - t.oslp / t.sslp),
                                                                                                 Bound::Open, t.distXminus(), t.deltaX(), Bound::Open); })
                                            | iter::map([&](auto t) { return VT(t); }))
-                                         
                                  | iter_dbg(1, "ezero")
                        ; };
-                       auto eseg         = [&]() { 
-                         return 
+                       auto eseg         = [&]() {
+                         return
                            iter::if_then_(t.sslp == 0 || ( t.sslp < 0 && isIneq(symbol)), breaks_plus_epsilon())
                                  else_if_(t.sslp >  0 && symbol == PredSymbol::Geq, iter::concat(breaks_plus_epsilon(), ezero()))
                                  else_if_(t.sslp >  0 && symbol == PredSymbol::Gt , iter::concat(breaks_plus_epsilon(), ezero() | iter::map([](auto x) { return x + epsilon; })))
@@ -117,13 +116,13 @@ namespace viras {
                                  else_is_(t.sslp != 0 && symbol == PredSymbol::Eq,  ezero())
                                  | iter_dbg(1, "eseg")
                        ; };
-                       auto ebound_plus  = [&]() { return 
+                       auto ebound_plus  = [&]() { return
                            iter::if_then_(lit.lim(infty), iter::vals<VT>(t.distXplus(), t.distXplus() + epsilon))
                                 else____(                   iter::vals<VT>(t.distXplus()                         ))
                                  | iter_dbg(1, "ebound_plus")
                                  ; };
 
-                       auto ebound_minus = [&]() { return 
+                       auto ebound_minus = [&]() { return
                            iter::if_then_(lit.lim(-infty), iter::vals<VT>(t.distXminus(), -infty))
                                  else____(                   iter::vals<VT>(t.distXminus()        ))
                                  | iter_dbg(1, "ebound_minus")
@@ -132,19 +131,19 @@ namespace viras {
                        return iter::if_then_(t.periodic(), iter::concat(ebreak(), eseg()))
                                     else____(              iter::concat(ebreak(), eseg(), ebound_plus(), ebound_minus()));
                    }());
-
     }
 
     auto elim_set(Var<C> x, std::vector<LiraLiteral<C>> const& lits)
     { return iter::array(lits)
         | iter::flat_map([&](auto lit) { return elim_set(x, *lit); }); }
 
-    Literal<C> literal(Term<C> t, PredSymbol s) 
+    Literal<C> literal(Term<C> t, PredSymbol s)
     { return Literal<C> { &_config, _config.create_literal(t.inner, s), }; }
 
     Literal<C> literal(bool b) { return literal(term(0), b ? PredSymbol::Eq : PredSymbol::Neq); }
 
-    Literal<C> vsubs_aperiodic0(LiraLiteral<C> const& lit, Var<C> x, VirtualTerm<C> vt) {
+    Literal<C> vsubs_aperiodic(LiraLiteral<C> const& lit, Var<C> x, VirtualTerm<C> vt)
+    {
       auto impl = [&]() {
 
       auto& s = lit.term;
@@ -157,7 +156,7 @@ namespace viras {
           if (!vt.term) {
             vt.term = term(0);
           }
-          return vsubs_aperiodic0(lit, x, vt);
+          return vsubs_aperiodic(lit, x, vt);
         } else {
           return literal(lit.lim(*vt.infty));
         }
@@ -175,7 +174,7 @@ namespace viras {
           case PredSymbol::Geq:
           case PredSymbol::Gt: {
             /* case 5 */
-            return literal(s.lim_at(*vt.term), 
+            return literal(s.lim_at(*vt.term),
                   s.sslp > 0 ? PredSymbol::Geq
                 : s.sslp < 0 ? PredSymbol::Gt
                 :              symbol);
@@ -187,16 +186,17 @@ namespace viras {
       }
       };
       auto res = impl();
-      VIRAS_LOG(1, "substituting ", lit, "[ ", x, " // ", vt, " ] = ", res)
+      VIRAS_LOG(1, "substituting ( ", lit, " )[ ", x, " // ", vt, " ] = ", res)
       return res;
     }
 
 
-    auto vsubs_aperiodic1(std::vector<LiraLiteral<C>> const& lits, Var<C> x, VirtualTerm<C> term) {
+    auto vsubs_aperiodic(std::vector<LiraLiteral<C>> const& lits, Var<C> x, VirtualTerm<C> term)
+    {
       VIRAS_ASSERT(!term.period);
           /* case 2 */
-      return iter::array(lits) 
-        | iter::map([this, x, term](auto lit) { return vsubs_aperiodic0(*lit, x, term); });
+      return iter::array(lits)
+        | iter::map([this, x, term](auto lit) { return this->vsubs_aperiodic(*lit, x, term); });
     }
 
     auto vsubs(std::vector<LiraLiteral<C>> const& lits, Var<C> x, VirtualTerm<C> vt) {
@@ -210,26 +210,26 @@ namespace viras {
                                              | iter::map([&](auto L) { return L->term.per; })
                                              | iter::fold([](auto l, auto r) { return lcm(l, r); }));
 
-                              auto iGrid = [ vt](auto... args) { 
+                              auto iGrid = [ vt](auto... args) {
                               return intersectGrid(Break<C>(*vt.term, *vt.period), args...)
                                                   | iter::map([](auto x) { return VirtualTerm<C>(x); });
                               ; };
                               auto all_lim_top = [A](Infty inf) { return A() | iter::all([inf](auto l) { return l->lim(inf) == true; }); };
                               auto one_lambda_plus = [iGrid, vt, lambda](Infty inf) {
-                                return iGrid(Bound::Closed, *vt.term, lambda, Bound::Open) 
+                                return iGrid(Bound::Closed, *vt.term, lambda, Bound::Open)
                                      | iter::map([&](auto s) { return s + inf; });
                               };
                               std::optional<LiraLiteral<C> const*> aperiodic_equality;
-                              auto aperiodic_equality_exists = [&]() -> bool { 
+                              auto aperiodic_equality_exists = [&]() -> bool {
                                 aperiodic_equality = A()
                                                    | iter::filter([](auto l) { return l->symbol == PredSymbol::Eq; })
                                                    | iter::min_by_key([](auto l) { return l->term.deltaX(); });
                                 return bool(aperiodic_equality);
                               };
-                              auto fin = 
+                              auto fin =
                                 iter::if_then_(all_lim_top( infty), one_lambda_plus( infty))
                                       else_if_(all_lim_top(-infty), one_lambda_plus(-infty))
-                                      else_if_(aperiodic_equality_exists(), 
+                                      else_if_(aperiodic_equality_exists(),
                                                iGrid(Bound::Closed, (**aperiodic_equality).term.distXminus(), (**aperiodic_equality).term.deltaX(), Bound::Closed))
                                       else____(
                                             A()
@@ -237,11 +237,11 @@ namespace viras {
                                           | iter::flat_map([lambda, iGrid](auto L) { return iGrid(Bound::Closed, L->term.distXminus(), L->term.deltaX() + lambda, Bound::Closed); })
                                           )
                                          | iter_dbg(1, "fin");
-                              return std::move(fin) 
-                              | iter::map([this,&lits,x](auto t) { return vsubs_aperiodic1(lits, x, t); });
+                              return std::move(fin)
+                              | iter::map([this,&lits,x](auto t) { return this->vsubs_aperiodic(lits, x, t); });
                             }()))
                    else____( ([&]() {
-                         auto out = iter::vals(vsubs_aperiodic1(lits, x, vt));
+                         auto out = iter::vals(vsubs_aperiodic(lits, x, vt));
                          return out;
                        }()));
     }
