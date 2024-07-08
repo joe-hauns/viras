d57590b1e1ef97b0bb89c552a222da195c196f42
joe-hauns <joe-hauns@protonmail.com>
2024-07-08 15:37:46 +0200

    updated config macros for flexibility

--- src/viras/config_macros.h
+++ src/viras/config_macros.h
@@ -10,14 +10,16 @@
 #  define VIRAS_DEBUG_LEVEL 0
 #endif
 
-template<class... As>
-void __viras_log_all(As const& ... as) { (std::cout << ... << as); }
-#define __VIRAS_LOG(...) __viras_log_all("[ viras ] ", __FILE__, "\t@ ", __LINE__, ":\t", __VA_ARGS__, "\n");
+#ifndef  VIRAS_OUTPUT
+   template<class... As>
+   void __viras_log_all(As const& ... as) { (std::cout << ... << as); }
+
+#  define VIRAS_OUTPUT(...)  \
+     __viras_log_all("[ viras ] ", __FILE__, "\t@ ", __LINE__, ":\t", __VA_ARGS__, "\n");
+#endif
 
 
-#ifndef VIRAS_LOG
-#  define VIRAS_LOG(lvl, ...) if (lvl < VIRAS_DEBUG_LEVEL) { __VIRAS_LOG(__VA_ARGS__) }
-#endif 
+#define VIRAS_LOG(lvl, ...) if (lvl < VIRAS_DEBUG_LEVEL) { VIRAS_OUTPUT(__VA_ARGS__) }
 
 #ifndef DBGE
 #  define DBGE(expr) __VIRAS_LOG(#expr, " = ", expr)
